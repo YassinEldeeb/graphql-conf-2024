74d6670166dd3316a547cb6ac34d4c53df0216fd
YassinEldeeb <yassineldeeb94@gmail.com>
2024-09-12 09:41:47 -0700

    fix

--- app/App.tsx
+++ app/App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import BoxersList from './components/List'
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-})
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <BoxersList />
-        <StatusBar style='auto' />
-      </View>
-    </ApolloProvider>
+    <View style={styles.container}>
+      <BoxersList />
+      <StatusBar style='auto' />
+    </View>
   )
 }
 
